0696b715df6eff6b81dd41cd7a6cb1a35e39cf17
phelipequeiroz <phelipedaniel84@gmail.com>
2020-02-05 17:29:28 -0300

    layout site

--- components/HeaderSlide.vue
+++ components/HeaderSlide.vue
@@ -1,59 +1,31 @@
 <template>
   <div>
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="500"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="~assets/image/bannerindex-boleto.jpg"
-      ></b-carousel-slide>
-		
-    <!-- Text slides with image -->
-      <b-carousel-slide
-        img-src="~assets/image/bannerindex-ecommerce.jpg"
-      ></b-carousel-slide>
-    <!-- Text slides with image -->
-      <b-carousel-slide
-        img-src="~assets/image/bannerindex-cobrancas.jpg"
-      ></b-carousel-slide>
-      
-
-     
-
-     
-    </b-carousel>
+     <b-carousel
+    id="carousel-fade"
+    :interval="4000"
+    controls
+    indicators
+    background="#ababab"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption="First slide"
+      img-src="https://picsum.photos/1024/480/?image=10"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Second Slide"
+      img-src="https://picsum.photos/1024/480/?image=12"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Third Slide"
+      img-src="https://picsum.photos/1024/480/?image=22"
+    ></b-carousel-slide>
+  </b-carousel>
 
     
   </div>
 </template>
-
-<script>
-  export default {
-    data() {
-      return {
-        slide: 0,
-        sliding: null
-      }
-    },
-    methods: {
-      onSlideStart(slide) {
-        this.sliding = true
-      },
-      onSlideEnd(slide) {
-        this.sliding = false
-      }
-    }
-  }
-</script>
