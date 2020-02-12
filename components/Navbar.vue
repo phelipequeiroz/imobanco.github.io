1f116c556a78663a136ebc5fc1b1d5d0af37de87
phelipequeiroz <phelipedaniel84@gmail.com>
2020-02-11 23:59:34 -0300

    navbar vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,24 @@
     <section class="login color-gradient-login">
     <div class="container">
       <div class="row">
-        <div class="col-lg-12 p-0">
+        <div class="col-lg-12">
+        <b-navbar toggleable="lg" class="color-nav" type="dark" variant="info">
+    <router-link to="/" class="navbar-brand"><img src="~assets/image/logoimobanco.png" alt=""></router-link>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <ul class="navbar-nav">
+                <router-link to="/"><span class="imo-text-06 text-content pl-2 pr-2">Home</span></router-link>
+                <router-link to="/charges"><span class="imo-text-06 text-content pl-2 pr-2 ">Cobranças</span></router-link>
+                <router-link to="/slip"><span class="imo-text-06 text-content pl-2 pr-2">Boleto</span></router-link>
+                <router-link to="/ecommerce"><span class="imo-text-06 text-content pl-2 pr-2">E-Commerce</span></router-link>
+                <a href="https://blog.imobanco.com.br/" target="_blank"><span class="imo-text-06 text-content pl-2 pr-2">Blog</span></a>
+                <router-link to="/contact"><span class="imo-text-06 text-content pl-2 pr-2">Contato</span></router-link>
+                </ul> 
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+        <!-- <div class="col-lg-12 p-0">
           <nav class="navbar navbar-expand-lg navbar-dark color-nav">
              <router-link to="/" class="navbar-brand"><img src="~assets/image/logoimobanco.png" alt=""></router-link>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,15 +37,12 @@
               </div>
             </nav>
        
-  </div>
-
+  </div> -->
+      </div>
       </div>
     </div>
     </section>
   </template>
-  <script>
-  
-  </script>
 <style>
 
 
