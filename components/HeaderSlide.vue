906594a7f527088a8f602a8da77230f8eb7a1ec2
phelipequeiroz <phelipedaniel84@gmail.com>
2020-02-12 09:41:51 -0300

    background font size

--- components/HeaderSlide.vue
+++ components/HeaderSlide.vue
@@ -2,7 +2,7 @@
 <div class="slidemobile">
   <div class="container">
     <div class="row justify-content-left">
-      <div class="col-lg-6 col-md-8 col-sm-8 col-8 pt-2">
+      <div class="col-xl-6 col-lg-6 col-md-8 col-sm-8 col-8 pt-2">
         <h1 class="mt-5 text-content imo-text-07 font-size" style="text-shadow: white 0.1em 0.1em 0.2em"> Abra uma conta <span class="text-titulo imo-text-02">Imobanco</span> e tenha 
         <span class="text-titulo imo-text-02"> Gestão financeira </span>para o seu negócio</h1>
       </div>
@@ -68,13 +68,14 @@
   left: 210px;
 } */
 .font-size{
-  font-size: calc(2vw + 3vmin);
+  font-size: calc(1.7vw + 3vmin);
 
 }
 
 .slidemobile{
         height: 500px;
         background-image: url("~assets/image/bannerindexslide.jpg") !important;
+        background-color: #9c9c9c;
         background-size: 100% auto cover;
         background-position: center;
         background-repeat: no-repeat;
--- components/ecommerce/EcommerceTop.vue
+++ components/ecommerce/EcommerceTop.vue
@@ -2,7 +2,7 @@
 <div class="slidemobileecommerce">
   <div class="container">
     <div class="row justify-content-left">
-      <div class="col-lg-6 col-md-6 col-sm-6 col-8 pt-2">
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-8 pt-2">
         <h1 class="mt-5 text-content imo-text-07 font-size-ecommerce" style="text-shadow: white 0.1em 0.1em 0.2em"> Ganhe uma <span class="text-titulo imo-text-02">loja virtual</span> e feche negócios com seu<span class="text-titulo imo-text-02"> E-Commerce</span>
        </h1>
       </div>
@@ -20,11 +20,12 @@
 
 <style>
 .font-size-ecommerce{
-  font-size: calc(2vw + 3.5vmin);
+  font-size: calc(1.7vw + 3vmin);
 }
 .slidemobileecommerce{
         height: 500px;
         background-image: url("~assets/image/bannerindexslideecommerce.jpg") !important;
+        background-color: #9c9c9c;
         background-size: 100% auto cover;
         background-position: center;
         background-repeat: no-repeat;
