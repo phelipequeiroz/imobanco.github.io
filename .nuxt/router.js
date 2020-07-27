e302d25dcac1ec3f203de532e4deee0547184d37
Phelipe <phelipetrabalhosti@gmail.com>
2020-07-27 14:38:21 -0300

    sem os telefones no site

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,12 +3,12 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _836fe6f6 = () => interopDefault(import('../pages/charges/index.vue' /* webpackChunkName: "pages/charges/index" */))
-const _3382e4dc = () => interopDefault(import('../pages/cobrancas/index.vue' /* webpackChunkName: "pages/cobrancas/index" */))
-const _2b945726 = () => interopDefault(import('../pages/contact/index.vue' /* webpackChunkName: "pages/contact/index" */))
-const _258f7286 = () => interopDefault(import('../pages/ecommerce/index.vue' /* webpackChunkName: "pages/ecommerce/index" */))
-const _0fa0cc50 = () => interopDefault(import('../pages/slip/index.vue' /* webpackChunkName: "pages/slip/index" */))
-const _5a843155 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _0b6cb81e = () => interopDefault(import('../pages/charges/index.vue' /* webpackChunkName: "pages/charges/index" */))
+const _7ce6b6eb = () => interopDefault(import('../pages/cobrancas/index.vue' /* webpackChunkName: "pages/cobrancas/index" */))
+const _78b902bf = () => interopDefault(import('../pages/contact/index.vue' /* webpackChunkName: "pages/contact/index" */))
+const _8790c842 = () => interopDefault(import('../pages/ecommerce/index.vue' /* webpackChunkName: "pages/ecommerce/index" */))
+const _11386f57 = () => interopDefault(import('../pages/slip/index.vue' /* webpackChunkName: "pages/slip/index" */))
+const _ef52f424 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -28,27 +28,27 @@ export const routerOptions = {
 
   routes: [{
     path: "/charges",
-    component: _836fe6f6,
+    component: _0b6cb81e,
     name: "charges"
   }, {
     path: "/cobrancas",
-    component: _3382e4dc,
+    component: _7ce6b6eb,
     name: "cobrancas"
   }, {
     path: "/contact",
-    component: _2b945726,
+    component: _78b902bf,
     name: "contact"
   }, {
     path: "/ecommerce",
-    component: _258f7286,
+    component: _8790c842,
     name: "ecommerce"
   }, {
     path: "/slip",
-    component: _0fa0cc50,
+    component: _11386f57,
     name: "slip"
   }, {
     path: "/",
-    component: _5a843155,
+    component: _ef52f424,
     name: "index"
   }],
 
